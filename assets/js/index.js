1d5c6c6a041575a05a6f4ecab6544bd4606dbec0
iamjustkhan <khantoro110811@gmail.com>
2021-04-08 00:04:24 +0600

    menu_btn click eff

--- assets/js/index.js
+++ assets/js/index.js
@@ -53,3 +53,44 @@ window.onload = function () {
     }, 2000);
 
 }
+
+
+
+
+
+// menu link
+let linkNav = document.querySelectorAll('[href^="#"]'),
+    V = 0.2;
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        let w = window.pageYOffset,
+            hash = this.href.replace(/[^#]*(.*)/, '$1');
+        t = document.querySelector(hash).getBoundingClientRect().top,
+            start = null;
+        requestAnimationFrame(step);
+
+        function step(time) {
+            if (start === null) start = time;
+            let progress = time - start,
+                r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+            window.scrollTo(0, r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash
+            }
+        }
+    }, false);
+};
+
+
+
+const nav_links = document.querySelectorAll('.section-nav a');
+
+nav_links.forEach(element => {
+    element.addEventListener('click', function(){
+        nav.classList.remove('nav_show');
+        menu_btn.classList.remove('menu_btn_active');
+    })
+});
